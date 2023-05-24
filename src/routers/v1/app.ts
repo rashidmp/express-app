d7a90339d62c2687afb0939a164f496a909ef85b
Muhammed Rashid M P <alrashid4star@gmail.com>
2023-05-25 04:38:47 +0530

    feat: cloudinary  as storage provider ☁️

--- src/routers/v1/app.ts
+++ src/routers/v1/app.ts
@@ -1,7 +1,18 @@
 import { Request, Response, Router } from 'express';
 import { upload } from '../../middleware/upload';
 
+import dotenv from 'dotenv';
+dotenv.config();
+
+import { v2 as cloudinary } from 'cloudinary';
+
 const app: Router = Router();
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 app.get('/', (req: Request, res: Response) => {
   res.json({
@@ -10,12 +21,29 @@ app.get('/', (req: Request, res: Response) => {
   });
 });
 
-app.post('/upload', upload.single('file'), (req: Request, res: Response) => {
-  console.log(req.body);
-  res.json({
-    status: 200,
-    message: 'upload',
-  });
-});
+app.post(
+  '/upload',
+  upload.single('file'),
+  async (req: Request, res: Response) => {
+    try {
+      if (!req.file) throw 'No File not found!';
+      const result = await cloudinary.uploader.upload(req.file.path, {
+        folder: 'images',
+      });
+      console.log(`File uploaded successfully! ${result.url}`);
+      res.status(200).json({
+        status: 200,
+        message: 'File uploaded successfully!',
+        data: result,
+      });
+    } catch (error: any) {
+      console.log(error);
+      res.status(500).json({
+        status: 500,
+        message: error,
+      });
+    }
+  }
+);
 
 export const routerV1 = app;
